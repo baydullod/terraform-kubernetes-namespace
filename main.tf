00b82be9d058399095213d7ec97507d38b6ca6c2
baydullod <baydullod@gmail.com>
2023-11-22 18:23:11 -0600

    changes to output

--- main.tf
+++ main.tf
@@ -12,4 +12,8 @@ resource "kubernetes_namespace" "example" {
   }
 }
 
+output name {
+  description = "Prints out the name of the namespace"
+  value = kubernetes_namsepace.example.metadata.name
+}
 
